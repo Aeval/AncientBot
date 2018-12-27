479030af8d1ecac750341f4dbf07ec0cd61e3eff
Zackary Fisher <zfisher@thorlabs.com>
2018-12-27 15:26:54 -0500

    log args

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ client.on('message', async message => {
 
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+
+    console.log(args);
   }
 
   if (command === 'match') {
